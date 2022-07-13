9ebc41c30f5e1df5f1c90467f7c0dc29afe8c543
rosyloven <108280612+rosyloven@users.noreply.github.com>
2022-07-13 20:28:18 +0300

    Add files via upload

---
+++ thirdTask/script.js
@@ -0,0 +1,63 @@
+const arrCitizens = [
+    {
+    name: "Gregoriy Lapinskiy",
+    height: "175",
+    weight: "85",
+    hair_color: "brown",
+    birthyear: "#ffffff",
+    gender: "male"
+    }, 
+    {
+    name: "Victoria Morth",
+    birthyear: "#ffffff",
+    gender: "female"
+    }, 
+    {
+    name: "Vladislav Moretti",
+    height: "190.242",
+    weight: "101",
+    hair_color: "light-brown",
+    skin_color: "white",
+    eyecolor: "brown",
+    birthyear: "#ffffff",
+    gender: "male"
+    }, 
+    {
+    "name":"Biggs Darklighter",
+    "height":"183.98",
+    "wei_ght":"84",
+    "hair_color":"blue",
+    "skin_color":"dark",
+    "eye_color":"brown",
+    "birthyear":"#ffffff",
+    "gender":"male"
+    }, 
+    {
+    name: "Victoriiiia Morth",
+    birthyear: "#ffffff",
+    gender: "female"
+    }, 
+];
+const obj = {  
+    "name":"some name",
+    "height":"182",
+    "mass":"77",
+    "hair_color":"black",
+    "skin_color":"red",
+    "eye_color":"brown",
+    "birth_year":"XXXXXX",
+    "gender":"female"
+}
+
+const getCombinedArray = (array, object) => {
+    const newArray = console.log(array.slice(array.splice(2, 0, object)));
+    return newArray;
+    }
+getCombinedArray(arrCitizens, obj);
+
+
+
+
+
+
+
