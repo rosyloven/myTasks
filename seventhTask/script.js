4bb5b01ba5a9f705afa4b80150c5ae746ef97ddd
rosyloven <108280612+rosyloven@users.noreply.github.com>
2022-07-18 12:38:33 +0300

    Add files via upload

---
+++ seventhTask/script.js
@@ -0,0 +1,67 @@
+const arrCitizens = [
+    {
+    name: "Gregoriy Lapinskiy",
+    height: "175",
+    weight: "85",
+    hair_color: "brown",
+    birthyear: "#ffffff",
+    gender: "male",
+    }, 
+    {
+    name: "Victoria Morth",
+    birthyear: "#ffffff",
+    gender: "female",
+    }, 
+    {
+    name: "Vladislav Moretti",
+    height: "190.242",
+    weight: "101",
+    hair_color: "light-brown",
+    skin_color: "white",
+    eyecolor: "brown",
+    birthyear: "#ffffff",
+    gender: "male",
+    }, 
+    {
+    name: "Vladislav Moretti",
+    birthyear: "#ffffff",
+    gender: "male",
+    }, 
+    {
+    "name":"Biggs Darklighter",
+    "height":"183.98",
+    "wei_ght":"84",
+    "hair_color":"blue",
+    "skin_color":"dark",
+    "eye_color":"brown",
+    "birthyear":"#ffffff",
+    "gender":"male",
+    }, 
+    {
+    name: "Anna Marsh",
+    birthyear: "#ffffff",
+    gender: "female",
+    }, 
+];
+const deleteObjByHeight = (array, objHeight) => {
+    return [...array].reduce((names, user) => {
+        if (Number(user.height) !== objHeight) {
+            names.push(user);
+        }
+        return names;
+    }, [])
+}
+console.log(deleteObjByHeight(arrCitizens, 175));
+
+
+
+
+
+
+
+/* Шестое задание
+const sortFromIndex = (array, sortIndexFrom, sortIndexTo) => {
+    return array.slice(sortIndexFrom, sortIndexTo)
+}
+console.log(sortFromIndex(arrCitizens, 2, 5));
+*/
