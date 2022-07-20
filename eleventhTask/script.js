1b1a45f9e4902a54468532410af0d61782ab417d
rosyloven <108280612+rosyloven@users.noreply.github.com>
2022-07-20 11:19:39 +0300

    Add files via upload

---
+++ eleventhTask/script.js
@@ -0,0 +1,53 @@
+const arrCitizens = [
+    {
+    name: "Gregoriy Lapinskiy",
+    height: 175,
+    weight: 85,
+    hair_color: "brown",
+    birthyear: "#ffffff",
+    gender: "male",
+    }, 
+    {
+    name: "Victoria Morth",
+    birthyear: "#ffffff",
+    gender: "female",
+    }, 
+    {
+    name: "Vladislav Moretti",
+    height: 190.242,
+    weight: 101,
+    hair_color: "light-brown",
+    skin_color: "white",
+    eyecolor: "brown",
+    birthyear: "#ffffff",
+    gender: "male",
+    }, 
+    {
+    name: "Vladislav Moretti",
+    birthyear: "#ffffff",
+    gender: "male",
+    }, 
+    {
+    "name":"Biggs Darklighter",
+    "height":183.98,
+    "wei_ght":84,
+    "hair_color":"blue",
+    "skin_color":"dark",
+    "eye_color":"brown",
+    "birthyear":"#ffffff",
+    "gender":"male",
+    }, 
+    {
+    name: "Anna Marsh",
+    birthyear: "#ffffff",
+    gender: "female",
+    }, 
+];
+
+const arrayToString = (array) => array.map(person => {
+    return Object.values(person).filter((val) => typeof val === 'string').join(', ');
+});
+console.log(arrayToString(arrCitizens));
+
+    //.reduce((sum, current) => sum + current, 0);console.log(prepearedString)
+    //return [`${prepearedString}, ${person}`];
